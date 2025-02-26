967e212829776ca094f43478b5484b5fcbda0484
Prashanth <nagaprashanth@sutantra-software.com>
2025-02-26 10:49:06 +0530

    API integration for dashboard in betacode

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  private isLoggedIn = false;
+  private router = inject(Router);
 
+  logout() {
+    this.isLoggedIn = false;
+    localStorage.removeItem('isLoggedIn'); // Clear login state
+    this.router.navigate(['/login']);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/betacode/api',
+  apiUrl: 'https://betacode-backend-1.onrender.com/betacode/api',
+  apiUrlLocal: 'http://localhost:4000/betacode/api',
 };
