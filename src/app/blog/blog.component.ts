42a45ed46f1736876a7b24f0bd7acac4525f0094
Prashanth <nagaprashanth@sutantra-software.com>
2025-03-01 22:54:50 +0530

    Add blog api integration in betacode

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { BlogService } from './blog.service';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent {
+  title: any = '';
+  date: any = '';
+  description: string = '';
+  image: any = '';
+  likes: string = '';
+  file!: File | null;
 
+  constructor(private service: BlogService) {}
+
+  resetForm() {
+    this.title = '';
+    this.date = '';
+    this.description = '';
+    this.image = '';
+    this.likes = '';
+  }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0]; // Get the first file
+    if (file) {
+      this.file = file;
+    }
+  }
+
+  convertFileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+      reader.readAsDataURL(file);
+    });
+  }
+
+  async submitBlog() {
+    if (this.file) {
+      const base64Image = await this.convertFileToBase64(this.file);
+      this.image = base64Image;
+      const blogData = {
+        title: this.title,
+        date: this.date,
+        description: this.description,
+        image: this.image,
+        likes: this.likes,
+      };
+      this.service
+        .addBlog(blogData)
+        // .pipe(takeUntil(this.destroy$))
+        .subscribe((res) => {
+          console.log(res);
+          this.resetForm();
+        });
+    }
+  }
 }
